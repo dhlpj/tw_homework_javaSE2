acc695879742b1b317f386c56ab971306feec53a
Jack <773411997@qq.com>
2019-05-18 09:23:27 +0800

    finish ReduceTest

--- src/main/java/com/thoughtworks/collection/Reduce.java
+++ src/main/java/com/thoughtworks/collection/Reduce.java
@@ -2,7 +2,9 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Reduce {
 
@@ -13,42 +15,78 @@ public class Reduce {
     }
 
     public int getMaximum() {
-        throw new NotImplementedException();
+        return arrayList.stream().reduce(Integer::max).get();
     }
 
     public double getMinimum() {
-        throw new NotImplementedException();
+        return arrayList.stream().reduce(Integer::min).get();
     }
 
     public double getAverage() {
-        throw new NotImplementedException();
+        return arrayList.stream().mapToDouble(item->Double.valueOf(item.toString())).average().getAsDouble();
     }
 
     public double getOrderedMedian() {
-        throw new NotImplementedException();
+        Integer mid = arrayList.size()/2;
+        if(arrayList.size()%2==0){
+            return (double)(arrayList.get(mid-1)+arrayList.get(mid))/2;
+        }else{
+            return arrayList.get(mid);
+        }
     }
 
     public int getFirstEven() {
-        throw new NotImplementedException();
+        for(Integer item:arrayList){
+            if(item%2==0)
+                return item;
+        }
+        return 0;
     }
 
     public int getIndexOfFirstEven() {
-        throw new NotImplementedException();
+        for (int i = 0; i < arrayList.size(); i++) {
+            if (arrayList.get(i)%2==0){
+                return i;
+            }
+        }
+        return -1;
     }
 
     public boolean isEqual(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        if(this.arrayList.size()!=arrayList.size()){
+            return false;
+        }
+        List<Integer> collection1 = this.arrayList.stream().sorted(Comparator.naturalOrder()).collect(Collectors.toList());
+        List<Integer> collection2 = this.arrayList.stream().sorted(Comparator.naturalOrder()).collect(Collectors.toList());
+        for (int i = 0; i < collection1.size(); i++) {
+            if(collection1.get(i)!=collection2.get(i)){
+                return false;
+            }
+        }
+        return true;
     }
 
     public Double getMedianInLinkList(SingleLink singleLink) {
-        throw new NotImplementedException();
+        return getOrderedMedian();
     }
 
     public int getLastOdd() {
-        throw new NotImplementedException();
+        int lastOdd = -1;
+        for (int i = 0; i < arrayList.size(); i++) {
+            if (arrayList.get(i)%2!=0){
+                lastOdd = arrayList.get(i);
+            }
+        }
+        return lastOdd;
     }
 
     public int getIndexOfLastOdd() {
-        throw new NotImplementedException();
+        int index = -1;
+        for (int i = 0; i < arrayList.size(); i++) {
+            if (arrayList.get(i)%2!=0){
+                index = i;
+            }
+        }
+        return index;
     }
 }
